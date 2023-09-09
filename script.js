9736f3f9a4859e1726f1450baade4c122e599c90
ShamrezAli <shamrezsyedali@gmail.com>
2023-09-09 14:51:48 -0400

    Add functionality of creating single-border for div

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.body.onload = addElement;
 
-function addElement() {
+/* function addElement() {
     for (let i=0; i<16; i++) {
         const newDiv = document.createElement("div");
         const divBorder = newDiv.style.border = "thick solid #0000FF"
@@ -8,5 +8,32 @@ function addElement() {
         const currentDiv = document.getElementById("mainGrid");
         document.body.insertBefore(newDiv, currentDiv);
     }
-    
+} */
+
+/* function testBorder() {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "testID");
+    document.getElementById("testID").style.border = "thick solid #0000FF";
+    const newContent = document.createTextNode("div");
+    newDiv.appendChild(newContent)
+    const currentDiv = document.getElementById("mainGrid");
+    document.body.insertBefore(newDiv, currentDiv);
 }
+ */
+
+function addElement() {
+    // create a new div element
+    const newDiv = document.createElement("div");
+  
+    // and give it some content
+    const newContent = document.createTextNode("Hi there and greetings!");
+
+    newDiv.style.border = "thick solid #0000FF";
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
