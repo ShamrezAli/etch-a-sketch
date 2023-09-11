bd079f4bde18dbc135dbd80c1ef13083a1b4c426
ShamrezAli <shamrezsyedali@gmail.com>
2023-09-11 19:42:36 -0400

    Create a row of boxes upon entering a number

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 document.body.onload = addElement;
 
-/* function addElement() {
-    for (let i=0; i<16; i++) {
-        const newDiv = document.createElement("div");
-        const divBorder = newDiv.style.border = "thick solid #0000FF"
-        newDiv.appendChild(divBorder);
-        const currentDiv = document.getElementById("mainGrid");
-        document.body.insertBefore(newDiv, currentDiv);
-    }
-} */
-
-/* function testBorder() {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "testID");
-    document.getElementById("testID").style.border = "thick solid #0000FF";
-    const newContent = document.createTextNode("div");
-    newDiv.appendChild(newContent)
-    const currentDiv = document.getElementById("mainGrid");
-    document.body.insertBefore(newDiv, currentDiv);
-}
- */
+let gridNum = 13;
 
 function addElement() {
-    // create a new div element
-    const newDiv = document.createElement("div");
-  
-    // and give it some content
-    const newContent = document.createTextNode("Hi there and greetings!");
 
-    newDiv.style.border = "thick solid #0000FF";
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-  }
+    for (let i = 0; i<gridNum; i++) {
+        // create a new div element
+        const newDiv = document.createElement("div");
+
+        newDiv.setAttribute("class", "theGrids")
+    
+        // and give it some content
+        const newContent = document.createTextNode("Hi there and greetings!");
+
+        newDiv.style.border = "thick solid #0000FF";
+    
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+    
+        // add the newly created element and its content into the DOM
+        const currentDiv = document.getElementById("gridContainer");
+
+        currentDiv.appendChild(newDiv);
+    }
+}
