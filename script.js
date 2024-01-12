546e39f2dda5ee63291e78d51f56894937b0ee18
ShamrezAli <shamrezsyedali@gmail.com>
2024-01-11 23:02:05 -0500

    Make progress on squares in grid

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ document.body.onload = addElement;
 let gridInputNum = 3; // How many squares comprise width/height
 let gridNum = Math.pow(gridInputNum, 2); // Square the value to get square
 
-newWidth = gridInputNum*16; // Each square is 35px, so the total width should be the number of squares times 35px to get a proper square
-//document.getElementById("gridContainer").style.width = newWidth + "px"; // turn the newwidth value into the number of pixels with 'px'
+newWidth = (100/(gridInputNum + 1)); // Each square is 35px, so the total width should be the number of squares times 35px to get a proper square
+document.getElementById("theGrids").style.width = newWidth + "%"; // turn the newwidth value into the number of pixels with 'px'
 
+
+// the above is not applying my newWidth to the class.
+// If it did then I would have a perfect grid created
 
 function addElement() {
 
@@ -13,7 +16,7 @@ function addElement() {
         // create a new div element
         const newDiv = document.createElement("div");
 
-        newDiv.setAttribute("class", "theGrids")
+        newDiv.setAttribute("Id", "theGrids")
     
         // and give it some content
         const newContent = document.createTextNode("");
