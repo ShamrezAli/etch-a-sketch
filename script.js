86a9c8ed55b2e62de63d7d32f140af9cbf835b57
ShamrezAli <shamrezsyedali@gmail.com>
2024-01-13 16:06:14 -0500

    Lay future groundwork for setting up grid

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.body.onload = addElement;
 
-let gridInputNum = 3; // How many squares comprise width/height
+let gridInputNum = 0; // How many squares comprise width/height
 let gridNum = Math.pow(gridInputNum, 2); // Square the value to get square
 
 newWidth = (100/(gridInputNum + 1)); // Each square is 35px, so the total width should be the number of squares times 35px to get a proper square
-document.getElementById("theGrids").style.width = newWidth + "%"; // turn the newwidth value into the number of pixels with 'px'
+document.getElementById("theGrids").style.width = newWidth + "%";; // turn the newwidth value into the number of pixels with 'px'
+
+//document.body.style.backgroundColor = "red"; // turn the newwidth value into the number of pixels with 'px'
+
 
 
 // the above is not applying my newWidth to the class.
@@ -16,7 +19,7 @@ function addElement() {
         // create a new div element
         const newDiv = document.createElement("div");
 
-        newDiv.setAttribute("Id", "theGrids")
+        newDiv.setAttribute("class", "theGrids")
     
         // and give it some content
         const newContent = document.createTextNode("");
@@ -32,3 +35,15 @@ function addElement() {
         currentDiv.appendChild(newDiv);
     }
 }
+
+/* 
+What needs to be done:
+Add number chooser
+Make number chooser value change the grid input number value
+create a function: when button pressed, change the grid input number
+variable value (into the number in input box) and apply the 
+document theGrids style width equal to the newWidth calculated value 
+
+
+
+*/
